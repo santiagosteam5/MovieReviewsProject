0e1faa4f3652db3f405728f2e6025b2679a9720b
Santiagosteam5 <facsonnmartens@gmail.com>
2024-07-31 12:21:24 -0500

    Final commit

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page<h1>')
-    return render(request, 'home.html', {'name': 'Santiago Gomez'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies, 'name': 'Santiago Gomez'})
 
 def about(request):
     return render(request, 'about.html')
