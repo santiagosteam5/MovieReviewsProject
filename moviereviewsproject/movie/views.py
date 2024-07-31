aa5566e8aa28a5fd3571520a4a780668508fd86a
Santiagosteam5 <facsonnmartens@gmail.com>
2024-07-31 10:57:39 -0500

    Added about function

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page<h1>')
     return render(request, 'home.html', {'name': 'Santiago Gomez'})
+
+def about(request):
+    return render(request, 'about.html')
