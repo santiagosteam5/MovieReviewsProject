02ec530ff431da01f4a7cbd0969c135298379544
santiagosteam5 <46015873+santiagosteam5@users.noreply.github.com>
2024-08-21 09:26:16 -0500

    Merged local

---
+++ moviereviewsproject/movie/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page<h1>')
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies, 'name': 'Santiago Gomez'})
+
+def about(request):
+    return render(request, 'about.html')
